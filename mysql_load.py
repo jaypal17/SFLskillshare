18c1a7ed594ac3b31de7c9cee1f07a50038af658
Justin Pallett <justpallett@gmail.com>
2021-07-03 22:05:54 -0400

    Add files via upload

---
+++ mysql_load.py
@@ -0,0 +1,19 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="dbuser1",
+  password="Blue1234",
+  database="students"
+)
+
+mycursor = mydb.cursor()
+
+sql = "INSERT INTO student_info (username, password, firstname, email, dob, zip)
+VALUES (%s, %s)"
+val = ("LN1J", "Xmmrtuv1877", "Larry", "larry1zp@yahoo.com", "8/16/1993", "1991")
+mycursor.execute(sql, val)
+
+mydb.commit()
+
+print(mycursor.rowcount, "record inserted.")
