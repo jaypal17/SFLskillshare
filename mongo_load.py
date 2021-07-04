8bccf9cd15c0685c43700d0ddcbb3fcc53a0a33f
Justin Pallett <justpallett@gmail.com>
2021-07-04 11:33:25 -0400

    Update mongo_load.py
    added create collection

--- mongo_load.py
+++ mongo_load.py
@@ -2,8 +2,11 @@ import pymongo
 
 connect = pymongo.MongoClient("mongodb://localhost:27017/")
 mydb = connect["students"]
+#creates collection
 collection = mydb["student_info"]
-
+index_name = 'my_index'
+collection.create_index(name=index_name, unique=False)
+#loads data into collection
 loaddata = {"username": "KUI8", "password": "Hinll66", "firstname": "Kevin", "email": "Kbomb10@aol.com" 
 "dob": "6/7/1989" "zip": "8090"}
 
