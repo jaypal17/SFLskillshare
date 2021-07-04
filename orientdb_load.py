7cdb4dd94fd90f9c5889fd6faf5c9ada756db456
Justin Pallett <justpallett@gmail.com>
2021-07-04 13:32:01 -0400

    Update orientdb_load.py

--- orientdb_load.py
+++ orientdb_load.py
@@ -7,6 +7,4 @@ session_id = client.connect( "root", "Blue1234" )
 client.db_open( student_records, "root", "Blue1234" ) 
 
 #insert record 
-client.command("INSERT INTO Records "
-      "('username', 'record', 'gpa', 'studentID', 'phoneNum') "
-      "VALUES('KUI8', '0001', '%3.5', '2001', '123-865-9901')") 
+client.command("INSERT INTO Records ('username', 'record', 'gpa', 'studentID', 'phoneNum') VALUES('KUI8', '0001', '%3.5', '2001', '123-865-9901')") 
