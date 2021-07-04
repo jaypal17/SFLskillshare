4531f8da2e5fdc99697c7b6569519bc743102bc1
Justin Pallett <justpallett@gmail.com>
2021-07-04 11:35:24 -0400

    Update mongo_load.py
    missing commas

--- mongo_load.py
+++ mongo_load.py
@@ -7,7 +7,7 @@ collection = mydb["student_info"]
 index_name = 'my_index'
 collection.create_index(name=index_name, unique=False)
 #loads data into collection
-loaddata = {"username": "KUI8", "password": "Hinll66", "firstname": "Kevin", "email": "Kbomb10@aol.com" 
-"dob": "6/7/1989" "zip": "8090"}
+loaddata = {"username": "KUI8", "password": "Hinll66", "firstname": "Kevin", "email": "Kbomb10@aol.com", 
+"dob": "6/7/1989", "zip": "8090"}
 
 x = collection.insert_one(loaddata)
