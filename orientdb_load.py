d4ddd79e1987dca3382d7dae83e8b5937bb579b7
Justin Pallett <justpallett@gmail.com>
2021-07-04 13:07:32 -0400

    Update orientdb_load.py

--- orientdb_load.py
+++ orientdb_load.py
@@ -1,11 +1,12 @@
-//create connection 
+import pyorient
+#Create connection 
 client = pyorient.OrientDB("localhost", 2424) 
 session_id = client.connect( "root", "Blue1234" )
  
-//open databse 
+#open databse 
 client.db_open( student_records, "root", "Blue1234" ) 
 
-//insert record 
+#insert record 
 client.command("INSERT INTO Records "
       "('username', 'record', 'gpa', 'studentID', 'phoneNum') "
       "VALUES('KUI8', '0001', '%3.5', '2001', '123-865-9901')") 
